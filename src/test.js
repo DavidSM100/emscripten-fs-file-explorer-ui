b9ddfec8a4077a81266328255eada51130810db3
DavidSM100 <davidsm@systemli.org>
2025-07-13 16:23:19 -0400

    refactor: import the lib from `lib/index.js` when testing

--- src/test.js
+++ src/test.js
@@ -1,14 +1,12 @@
-import { mount } from "svelte";
-import App from "./lib/App.svelte";
+import "./lib/index.js";
 
 async function start() {
   const ioquake3 = await import("../ioquake3_opengl2.wasm32.js");
   const module = await ioquake3.default();
-  mount(App, {
-    target: document.getElementById("app"),
-    // @ts-ignore
-    props: { FS: module.FS },
-  });
+
+  const container = document.getElementById("app");
+  //@ts-ignore
+  window.openFileExplorer(container, module.FS);
 }
 
 start();
