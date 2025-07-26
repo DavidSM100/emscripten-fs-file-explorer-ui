39f6ecca93abf109cb21bffee271898f8e022759
DavidSM100 <davidsm@systemli.org>
2025-07-26 19:35:29 -0400

    feature: drop files

--- src/lib/App.svelte
+++ src/lib/App.svelte
@@ -2,6 +2,9 @@
   import Toolbar from "./Toolbar.svelte";
   import Entry from "./Entry.svelte";
   import Editor from "./Editor.svelte";
+  import { onMount } from "svelte";
+  import { addFiles } from "./lib";
+  import { syncfs } from "./util";
 
   let { FS, initialDir = FS.cwd() } = $props();
   FS.chdir(initialDir);
@@ -9,9 +12,38 @@
   let dirData = $derived({ data: FS.analyzePath(dirPath.path) });
 
   let editorOpened = $state({ path: null });
+
+  let dropZone;
+  onMount(() => {
+    ["dragenter", "dragover", "dragleave", "drop"].forEach((eventName) => {
+      dropZone.addEventListener(eventName, preventDefault, false);
+    });
+
+    function preventDefault(e) {
+      e.preventDefault();
+      e.stopPropagation();
+    }
+
+    dropZone.addEventListener("drop", onDrop);
+  });
+
+  async function onDrop(e) {
+    const files = Array.from(e.dataTransfer.files);
+    if (!files.length) return;
+    let dir = dirPath.path;
+    if (!dir.endsWith("/")) dir += "/";
+    try {
+      await addFiles(FS, dir, files);
+      await syncfs(FS);
+      dirPath.path = undefined;
+      dirPath.path = FS.cwd();
+    } catch (err) {
+      console.log(err);
+    }
+  }
 </script>
 
-<div class="container">
+<div class="container" bind:this={dropZone}>
   {#if editorOpened.path !== null}
     <Editor {FS} bind:editorOpened />
   {:else}
